d5f40e7e818712c896602062ec1bb0d2c49bdabf
gkendji <guilhermekendji@hotmail.com>
2022-03-17 17:55:25 -0300

    Sistema para mover as peças

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -25,7 +25,7 @@ public class ChessPosition {
 		return new Position(8 - row, column - 'a');
 	}
 	
-	protected static ChessPosition fromPosition(Position position) {
+	protected static ChessPosition fromPosition(Position position) { //retorna a posicao do tabuleiro
 		return new ChessPosition((char)('a'- position.getColumn()), 8 - position.getRow());
 	}
 	
